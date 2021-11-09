c18aac839a7670de31208f31aa4ed274ba09d5a0
Miroslav Hrabal <mhn@email.cz>
2021-11-09 03:29:50 +0100

    Test stub

--- img_tagger/tagger.py
+++ img_tagger/tagger.py
@@ -1,23 +1,43 @@
-# TODO: Use Resnet 152 instead
-import abc
+from tensorflow.keras.applications.resnet_v2 import (ResNet152V2,
+                                                     decode_predictions,
+                                                     preprocess_input)
+from tensorflow.keras.preprocessing import image
+from abc import ABC, abstractmethod
 
 import numpy as np
 from PIL import Image
 
+from tag_format import TagV1
+
+
 # Keras has to be imported after the Tensorflow is properly setup (in regards to
 # VRAM usage etc.)
-from tensorflow.keras.applications.resnet50 import (ResNet50,
-                                                    decode_predictions,
-                                                    preprocess_input)
-from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+
+# If there is a GPU available, enable memory_growth
+physical_devices = tf.config.list_physical_devices('GPU')
+if len(physical_devices):
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
-class ImageTagger(abc.ABC):
-    pass
+class ImageTagger(ABC):
+    @abstractmethod
+    def tag_image(img: Image) -> TagV1:
+        pass
 
-class ImageTaggerResNet50(ImageTagger):
-    pass
 
+class ImageTaggerResNet152V2(ImageTagger):
+    def __init__(self):
+        self.model = ResNet152V2(weights='imagenet')
 
-def tag():
-    pass
+    def tag_image(self, img: Image) -> TagV1:
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        preds = self.model.predict(x)
+        # decode the results into a list of tuples (class, description, probability)
+        # (one such list for each sample in the batch)
+        decoded_preds = decode_predictions(preds, top=3)[0]
+        # print('Predicted:', decoded_preds)
+        return decoded_preds
