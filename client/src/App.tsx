3d05d42fa1e8011694f310505d53d899d591b17f
Miroslav Hrabal <mhn@email.cz>
2021-11-24 22:44:10 +0100

    MUI demo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './App.css';
+import { Container, Divider, Drawer, IconButton, InputBase, List, ListItem, ListItemText, Toolbar } from '@mui/material';
+
+function SearchBar() {
+  return (
+    <Box>
+      <Paper style={{ display: "flex", alignItems: "center" }} sx={{ m: 1 }}>
+        <TextField sx={{ m: 1, flex: 1 }} placeholder="Search"></TextField>
+        <Button>Search</Button>
+      </Paper>
+    </Box>
+  );
+}
+
+function SearchView() {
+  return (
+    <Box>
+      <SearchBar />
+    </Box>
+  );
+}
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  const drawerWidth = 240;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={darkTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <header className="App-header">
+        </header>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="permanent"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Video Search
+            </Typography>
+          </Toolbar>
+
+          <Divider />
+          <List>
+            <ListItem button key={"Search by tag"}>
+              <ListItemText primary={"Search by tag"} />
+            </ListItem>
+          </List>
+          <Divider />
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            overflow: 'auto',
+          }}
+        >
+          <SearchView />
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
