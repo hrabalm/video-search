821b359576f3c4351d6aa204f548e9a230296b38
Miroslav Hrabal <mhn@email.cz>
2021-11-30 09:15:16 +0100

    Add components missing from the repo

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+
+export default function SearchBar() {
+    return (
+        <Box>
+            <Paper style={{ display: "flex", alignItems: "left", flexDirection: "column" }} sx={{ p: 1 }}>
+                <Box style={{ display: "flex", flexDirection: "row" }}>
+                    <TextField sx={{ m: 1, flex: 1 }} placeholder="Search"></TextField>
+                    <Button>Search</Button>
+                </Box>
+                <Box style={{ display: "flex", flexDirection: "row" }}>
+                    <Typography>Selected tags:</Typography>
+                </Box>
+            </Paper>
+        </Box>
+    );
+}
---
+++ client/src/components/SearchView.tsx
@@ -0,0 +1,10 @@
+import Box from '@mui/material/Box';
+import SearchBar from './SearchBar';
+
+export default function SearchView() {
+    return (
+        <Box>
+            <SearchBar />
+        </Box>
+    );
+}
