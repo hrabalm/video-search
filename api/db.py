03d86df51d0a49871ed07cfd38d312605882772a
Miroslav Hrabal <mhn@email.cz>
2022-01-03 23:08:25 +0100

    api: work on models

--- api/db.py
+++ api/db.py
@@ -1,6 +1,8 @@
 import os
 
 import psycopg2
+import psycopg2.extras
+import pypika.queries
 from pypika import Field, Query, Table
 
 DB_HOST = os.getenv("SQL_HOST")
@@ -17,3 +19,37 @@ conn = psycopg2.connect(
     host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD
 )
 cursor = conn.cursor()
+dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+def create_tables():
+    q = """
+        DROP TABLE IF EXISTS videos CASCADE;
+        DROP TABLE IF EXISTS tags CASCADE;
+        DROP TABLE IF EXISTS videos_tags CASCADE;
+
+        CREATE TABLE IF NOT EXISTS videos (
+            id serial PRIMARY KEY,
+            filename text,
+            md5 text,
+            created timestamp,
+            metadata json
+        );
+
+        CREATE TABLE IF NOT EXISTS tags (
+            id serial PRIMARY KEY,
+            tag text,
+            tag_type text  -- redundant
+        );
+
+        CREATE TABLE IF NOT EXISTS video_tags (
+            video_id int references videos(id),
+            tag_id int references tags(id)
+        );
+
+        INSERT INTO tags (tag, tag_type) VALUES
+            ('cat', 'nn'),
+            ('dog', 'nn');
+    """
+    cursor.execute(q)
+    conn.commit()
+create_tables()
--- api/models.py
+++ api/models.py
@@ -1,13 +1,25 @@
-from db import conn, cursor
+from db import conn, cursor, dict_cur
+from pypika import Field, Query, Table, PostgreSQLQuery
+
 
 class Tables:
     @staticmethod
     def get_all():
-        cursor.execute("SELECT * FROM pg_catalog.pg_tables")
+        q = """SELECT * FROM pg_catalog.pg_tables"""
+        cursor.execute(q)
         x = cursor.fetchall()
         return {"tables": x}
 
 class Tags:
+    # @staticmethod
+    # def get_all():
+    #     return {"tags": ["cat", "dog", "horse", "zebra"]}
+
     @staticmethod
     def get_all():
-        return {"tags": ["cat", "dog", "horse", "zebra"]}
+        q = """SELECT id, tag, tag_type FROM tags"""
+        dict_cur.execute(q)
+        res = dict_cur.fetchall()
+        res = [dict(x) for x in res]
+        # return {"tags": ["cat", "dog", "horse", "zebra"], "raw": res}
+        return {"tags": res}
