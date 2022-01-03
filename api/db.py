b81eb67d07b0d8f7c71235fd66ba3b60995cfea0
Miroslav Hrabal <mhn@email.cz>
2022-01-04 00:02:57 +0100

    Try using mongo instead of PostgreSQL

--- api/db.py
+++ api/db.py
@@ -1,55 +1,11 @@
 import os
+import pymongo
 
-import psycopg2
-import psycopg2.extras
-import pypika.queries
-from pypika import Field, Query, Table
+_DB_HOST = os.getenv("SQL_HOST")
+_DB_PORT = os.getenv("SQL_PORT")
+_DB_NAME = os.getenv("SQL_DB")
+_DB_USER = os.getenv("SQL_USER")
+_DB_PASSWORD = os.getenv("SQL_PASSWORD")
 
-DB_HOST = os.getenv("SQL_HOST")
-DB_PORT = os.getenv("SQL_PORT")
-DB_NAME = os.getenv("SQL_DB")
-DB_USER = os.getenv("SQL_USER")
-DB_PASSWORD = os.getenv("SQL_PASSWORD")
-
-print(f"{DB_HOST=}")
-print(f"{DB_USER=}")
-print(f"{DB_PASSWORD=}")
-
-conn = psycopg2.connect(
-    host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD
-)
-cursor = conn.cursor()
-dict_cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-def create_tables():
-    q = """
-        DROP TABLE IF EXISTS videos CASCADE;
-        DROP TABLE IF EXISTS tags CASCADE;
-        DROP TABLE IF EXISTS videos_tags CASCADE;
-
-        CREATE TABLE IF NOT EXISTS videos (
-            id serial PRIMARY KEY,
-            filename text,
-            md5 text,
-            created timestamp,
-            metadata json
-        );
-
-        CREATE TABLE IF NOT EXISTS tags (
-            id serial PRIMARY KEY,
-            tag text,
-            tag_type text  -- redundant
-        );
-
-        CREATE TABLE IF NOT EXISTS video_tags (
-            video_id int references videos(id),
-            tag_id int references tags(id)
-        );
-
-        INSERT INTO tags (tag, tag_type) VALUES
-            ('cat', 'nn'),
-            ('dog', 'nn');
-    """
-    cursor.execute(q)
-    conn.commit()
-create_tables()
+_mongo_client = pymongo.MongoClient(_DB_HOST, int(_DB_PORT) if _DB_PORT else None)
+db = _mongo_client[_DB_NAME if _DB_NAME else 'default']
--- api/models.py
+++ api/models.py
@@ -1,30 +1,16 @@
-from db import conn, cursor, dict_cur
-from pypika import Field, Query, Table, PostgreSQLQuery
-
-
-class Tables:
-    @staticmethod
-    def get_all():
-        q = """SELECT * FROM pg_catalog.pg_tables"""
-        cursor.execute(q)
-        x = cursor.fetchall()
-        return {"tables": x}
+from db import db
 
 class Tags:
-    # @staticmethod
-    # def get_all():
-    #     return {"tags": ["cat", "dog", "horse", "zebra"]}
-
     @staticmethod
     def get_all():
-        q = """SELECT id, tag, tag_type FROM tags"""
-        dict_cur.execute(q)
-        res = dict_cur.fetchall()
-        res = [dict(x) for x in res]
-        # return {"tags": ["cat", "dog", "horse", "zebra"], "raw": res}
-        return {"tags": res}
+        tags = list(db.videos.distinct("tags"))
+        return {"tags": tags}
 
 class Videos:
     @staticmethod
-    def add_video(video):
-        raise NotImplementedError
+    def add(video):
+        db.videos.insert_one(video)
+
+    @staticmethod
+    def get_all():
+        return db.videos.find()
