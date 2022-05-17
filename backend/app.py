e0b626fdcd4a024fe73e7377eb8559cc716b205f
Miroslav Hrabal <mhn@email.cz>
2022-05-18 01:35:32 +0200

    backend: refactoring

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def api():
+    click.echo("Launching API development server...")
+    from api import app
+
+    app.app.run(debug=True)
+
+
+@cli.command()
+def image_tagger():
+    click.echo("Launching image tagger worker...")
+    raise NotImplementedError  # FIXME
+
+
+@cli.command()
+def video_indexer():
+    click.echo("Launching video indexer worker...")
+    raise NotImplementedError  # FIXME
+
+
+if __name__ == "__main__":
+    cli()
---
+++ backend/img_tagger/mocks.py
@@ -0,0 +1,37 @@
+# mock image
+
+from PIL import Image
+
+
+def mock_empty_image(size=(224, 224)) -> Image.Image:
+    return Image.new("RGB", size)
+
+
+def take_random_frames(video_file, count) -> list[Image.Image]:
+    def count_frames(container):
+        counter = 0
+        for _ in container.decode(video=0):
+            counter += 1
+        return counter
+
+    import random
+
+    import av
+
+    random.seed(42)
+    container = av.open(video_file)
+    # frames_count = container.streams.video[0].frames
+    frames_count = count_frames(container)
+    print(f"{frames_count=}")
+    selected_frames_indices = set(random.choices(range(frames_count), k=count))
+    selected_frames = []
+    container.seek(0)
+    frames = container.decode(video=0)
+    for i, frame in enumerate(frames):
+        if i in selected_frames_indices:
+            selected_frames.append(frame.to_image().resize((224, 224)))
+    return selected_frames
+
+
+# mock requests, jobs, responses
+# mock video
