06faf0202139ca552cbf253069d77a33122995d8
Miroslav Hrabal <mhn@email.cz>
2023-02-01 04:24:40 +0100

    fix: add missing videos component

---
+++ client/src/routes/videos.tsx
@@ -0,0 +1,19 @@
+import Title from "../components/Title";
+import {Form, useLoaderData } from "react-router-dom";
+import { getVideo } from "../lib/utils";
+
+export async function loader({ params }) {
+    const videoId = params.videoId;
+    return await getVideo(videoId);
+}
+
+export default function VideoDetail() {
+    const video = useLoaderData();
+    console.log(video);
+    return (
+        <>
+            <Title title="Video Detail" />
+            {JSON.stringify(video, null, 4)}
+        </>
+    );
+}
