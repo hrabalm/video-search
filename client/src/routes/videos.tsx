0a09ec43c763bbbef21a7bb3b1aaa76f9a78a4a1
Miroslav Hrabal <mhn@email.cz>
2023-02-26 02:35:58 +0100

    feat: fix tag summary layout

--- client/src/routes/videos.tsx
+++ client/src/routes/videos.tsx
@@ -1,12 +1,21 @@
 import Title from "../components/Title";
-import { Form, useLoaderData } from "react-router-dom";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 import { getVideo } from "../lib/utils";
-import { Chip, Grid, Paper, styled, TextField, Typography, Button } from "@mui/material";
+import {
+  Chip,
+  Grid,
+  Paper,
+  styled,
+  TextField,
+  Typography,
+  Button,
+} from "@mui/material";
 import _ from "lodash";
-import LinkButton from "../LinkButton";
 import DownloadButton from "../components/DownloadButton";
 
 const TAG_MIN_CONF = 0.75;
+const CHIP_MARGIN = 0.3;
 
 export async function loader({ params }: { params: any }) {
   const videoId = params.videoId;
@@ -21,14 +30,45 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+function TagsTable({ tags_by_tagger }: any) {
+  return (
+    <Grid container spacing={2}>
+      {Object.entries(tags_by_tagger).map(([model, tags], i) => {
+        return (
+          <React.Fragment key={i}>
+            <Grid item xs={4}>
+              <Item>
+                <Chip label={model} sx={{ margin: CHIP_MARGIN }} />
+              </Item>
+            </Grid>
+            <Grid item xs={8}>
+              <Item>
+                {tags
+                  .filter((tag) => tag.conf >= TAG_MIN_CONF)
+                  .map((tag) => {
+                    return (
+                      <Chip
+                        key={tag.tag}
+                        label={tag.tag}
+                        sx={{ margin: CHIP_MARGIN }}
+                      />
+                    );
+                  })}
+              </Item>
+            </Grid>
+          </React.Fragment>
+        );
+      })}
+    </Grid>
+  );
+}
+
 export default function VideoDetail() {
   const video: any = useLoaderData();
   const tags_by_tagger = _.groupBy(video.video.tags, (tag) => tag.model);
 
-  const CHIP_MARGIN = 0.3;
-
   // FIXME: this should be absolute URL to be useful
-  const base = "http://localhost:8080"// TODO: remove
+  const base = "http://localhost:8080"; // TODO: remove
   const url = base + `/api/v2/source-file/${video.video._id["$oid"]}`;
 
   return (
@@ -45,39 +85,35 @@ export default function VideoDetail() {
         // })
       }
       <Grid container>
-        <Grid xs sx={{m: 1}}>
-          <TextField id="url-textfield" label="Video URL" defaultValue={url} variant="outlined" size="small" InputProps={{ readOnly: true }} fullWidth onFocus={event => {
-        event.target.select();
-      }} />
+        <Grid item xs sx={{ m: 1 }}>
+          <TextField
+            id="url-textfield"
+            label="Video URL"
+            defaultValue={url}
+            variant="outlined"
+            size="small"
+            InputProps={{ readOnly: true }}
+            fullWidth
+            onFocus={(event) => {
+              event.target.select();
+            }}
+          />
         </Grid>
-        <Grid xs="auto" sx={{m: 1}}>
-          <Button variant="outlined" sx={{m: 0.5}} onClick={() => navigator.clipboard.writeText(url)}>Copy URL</Button>
+        <Grid item xs="auto" sx={{ m: 1 }}>
+          <Button
+            variant="outlined"
+            sx={{ m: 0.5 }}
+            onClick={() => navigator.clipboard.writeText(url)}
+          >
+            Copy URL
+          </Button>
         </Grid>
-        <Grid xs="auto" sx={{m: 1}}>
+        <Grid item xs="auto" sx={{ m: 1 }}>
           <DownloadButton text="Download" link={url} />
         </Grid>
       </Grid>
       <Typography>Tags:</Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <Item>
-            {Object.entries(tags_by_tagger).map(([model, tags]) => {
-              return <Chip label={model} sx={{ margin: CHIP_MARGIN }} />;
-            })}
-          </Item>
-        </Grid>
-        <Grid item xs={8}>
-          <Item>
-            {Object.entries(tags_by_tagger).map(([model, tags]) => {
-              return tags
-                .filter((tag) => tag.conf >= TAG_MIN_CONF)
-                .map((tag) => {
-                  return <Chip label={tag.tag} sx={{ margin: CHIP_MARGIN }} />;
-                });
-            })}
-          </Item>
-        </Grid>
-      </Grid>
+      <TagsTable tags_by_tagger={tags_by_tagger} />
     </>
   );
 }
