456e9528b101fe3c26a5e1e9ed617fe64a7bb5c8
Miroslav Hrabal <mhn@email.cz>
2023-03-07 06:53:50 +0100

    chore: look for new files periodically

--- backend/backend/tasks/periodic.py
+++ backend/backend/tasks/periodic.py
@@ -6,3 +6,13 @@ from backend.tasks import dramatiq
 @dramatiq.actor(periodic=periodiq.cron("0 * * * *"))
 def stub_hourly_task():
     pass
+
+
+@dramatiq.actor(periodic=periodiq.cron("* * * * *"))
+def index_new_files():
+    from backend.settings import settings
+    from backend.tasks.default import rpc_index_new_files
+
+    directories = list(settings.scanned_directories)
+    extensions = list(settings.video_extensions)
+    rpc_index_new_files.send(directories, extensions)
